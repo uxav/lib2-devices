17a1e0b44851a5978d9d6fe7b4826cd728edb086
Mike Jobson <mike@ux.digital>
2021-06-20 13:24:31 +0100

    Update CiscoTelePresenceCodec.cs
    Fix for logging error

--- Cisco/CiscoTelePresenceCodec.cs
+++ Cisco/CiscoTelePresenceCodec.cs
@@ -429,9 +429,9 @@ namespace UX.Lib2.Devices.Cisco
                     OnStatusReceived((from Match statusMatch in matches
                         select new StatusUpdateItem(statusMatch))
                         .ToArray());
+                    CloudLog.Notice("{0} Initialized OK", this);
                     if(_initialized) return;
                     _initialized = true;
-                    CloudLog.Notice("{0} Initialized OK", this);
                     break;
                 case ReceivedDataType.Event:
                     var items = new Dictionary<string, Dictionary<string, string>>();
